54aafa941549406f0be5a8ebc240189cfaa8e7e2
Gaurav <gaurav@mtouchlabs.com>
2021-01-09 22:48:32 +0530

    Flutter cart system

---
+++ android/app/src/main/kotlin/com/gaurav/flutter_ecommerce_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gaurav.flutter_ecommerce_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_provider/models/cart.dart';
+import 'package:flutter_ecommerce_provider/productlistview.dart';
+import 'package:flutter_ecommerce_provider/screens/cartview.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => Cart(),
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context)
+  {
+    var ccart = Provider.of<Cart>(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Products'),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Cartview()));
+                    },
+                  ),
+                  Text(ccart.count.toString())
+                ],
+              ),
+            )
+          ],
+          centerTitle: true,
+        ),
+        body: Center(child: ProductListView()));
+  }
+}
